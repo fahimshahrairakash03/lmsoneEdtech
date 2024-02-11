e8df67bed895373ada252d200a100eaf18411195
Fahim Shahriar Akash <fahimshahriarakash03@gmail.com>
2024-02-11 09:59:23 +0600

    header generated

---
+++ src/firebase/firebase.config.js
@@ -0,0 +1,19 @@
+// Import the functions you need from the SDKs you need
+import { initializeApp } from "firebase/app";
+// TODO: Add SDKs for Firebase products that you want to use
+// https://firebase.google.com/docs/web/setup#available-libraries
+
+// Your web app's Firebase configuration
+const firebaseConfig = {
+  apiKey: "AIzaSyDSQ_Ze0YW35nzfqAgqPSzCPPPR42yh080",
+  authDomain: "lmsone-edtech.firebaseapp.com",
+  projectId: "lmsone-edtech",
+  storageBucket: "lmsone-edtech.appspot.com",
+  messagingSenderId: "829610267360",
+  appId: "1:829610267360:web:cb45d0cee5ab312cd57685",
+};
+
+// Initialize Firebase
+const app = initializeApp(firebaseConfig);
+
+export default app;
